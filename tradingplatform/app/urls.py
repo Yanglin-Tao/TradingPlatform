44d2b2023734ea44e032d9a60c2caadd8784890c
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2024-07-29 12:03:40 -0400

    Make sign up api call

--- tradingplatform/app/urls.py
+++ tradingplatform/app/urls.py
@@ -5,7 +5,7 @@ from .views import stock_price
 
 urlpatterns = [
     path('price/<str:symbol>/', stock_price, name='stock_price'),
-    path('', views.login, name='home'),  
+    path('login/', views.login, name='login'),  
     path('signup/',views.signup,name = 'signup'),
     path('deleteaccount/',views.deleteAccount,name = 'delete_account'),
     path('userprofile/',views.getUserProfile,name = 'user_profile'),
--- tradingplatform/tradingplatform/settings.py
+++ tradingplatform/tradingplatform/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "app",
+    "corsheaders",
     # 'rest_framework_simplejwt.token_blacklist',
 ]
 
@@ -71,7 +72,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "tradingplatform.urls"
 
