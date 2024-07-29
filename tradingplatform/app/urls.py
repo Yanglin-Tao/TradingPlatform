55a9f8007c35b17d3e6b5431f6bef4ec2f770718
Leo Tao <leotao@Leos-MacBook-Pro.local>
2024-07-29 12:03:17 -0400

    add the delete account and view profile function

--- tradingplatform/app/urls.py
+++ tradingplatform/app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('price/<str:symbol>/', stock_price, name='stock_price'),
     path('', views.login, name='home'),  
     path('signup/',views.signup,name = 'signup'),
+    path('deleteaccount/',views.deleteAccount,name = 'delete_account'),
+    path('userprofile/',views.getUserProfile,name = 'user_profile'),
 ]
